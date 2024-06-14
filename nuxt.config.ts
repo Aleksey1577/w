8695fb5c47315396c80316daa4bd8dc51a95c7e1
Aleksey1577 <alekseykras94@gmail.com>
2024-06-14 17:02:41 +0300

    Setup GitHub Pages deployment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,15 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+// nuxt.config.ts
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
+  css: ["~/assets/css/main.css"],
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
+  },
+  ssr: false,
+  app: {
+    baseURL: "/w/", // Замените 'your-repository' на имя вашего репозитория
   },
 });
